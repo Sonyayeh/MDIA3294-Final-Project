a27d977b1e37d40d715bca48d250f415fe1e4426
Sonya <cyeh27@my.bcit.ca>
2024-12-11 13:05:52 -0800

    Layout update

--- Cat-Shop/src/Components/Layout.jsx
+++ Cat-Shop/src/Components/Layout.jsx
@@ -1,2 +1,18 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import Header from "./Header";
+import Footer from "./Footer";
+
+function Layout() {
+
+    return(
+        <section>
+            <Header />
+            <main>
+            <Footer />
+            </main>
+        </section>
+    );
+}
+
+export default Layout;
